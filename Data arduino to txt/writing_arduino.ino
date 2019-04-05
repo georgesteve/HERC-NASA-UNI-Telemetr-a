e9a973f500a05668441b65dc62f1463d45ca30d4
georgesteve <37008877+georgesteve@users.noreply.github.com>
2019-04-05 08:38:38 -0500

    Create writing_arduino.ino

---
+++ Data arduino to txt/writing_arduino.ino
@@ -0,0 +1,23 @@
+int potPin = 0;     // set pin for potentiometer
+
+int recordData[20]; // Array will store key input presses with 1 equal pressed and 0 equal not pressed
+char serInString[10]; 
+int value;
+void setup() {
+   //pinMode(ledPin, OUTPUT); 
+   Serial.begin(9600);
+   Serial.println("ready");
+}
+
+void loop() {
+
+
+     Serial.println("gg");
+  
+     delay(1000);
+
+   
+   delay(1000);
+}
+
+
